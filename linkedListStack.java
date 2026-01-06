86d9cadc5cc2d4c2e573dfc53272c3ea3c652a1c
SuneraJayalath <136885712+SuneraJayalath@users.noreply.github.com>
2026-01-06 11:46:26 +0530

    Create linkedListStack.java

---
+++ linkedListStack.java
@@ -0,0 +1,48 @@
+class LinkedListStack {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node top = null;
+
+    void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (top == null) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    void display() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.display();
+        System.out.println("Popped: " + stack.pop());
+        stack.display();
+    }
+}
