5793437642bedd62eddc24b205cfdc7f875570b6
SuneraJayalath <136885712+SuneraJayalath@users.noreply.github.com>
2026-01-20 06:33:09 +0530

    Create optimizedInsertionSortAlgorithm.java

---
+++ optimizedInsertionSortAlgorithm.java
@@ -0,0 +1,34 @@
+class OptimizedInsertionSort {
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+
+            
+            if (arr[i - 1] <= key)
+                continue;
+
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
